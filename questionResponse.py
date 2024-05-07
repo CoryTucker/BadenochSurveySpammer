97037d20052aa74c2571e5c8904ba2c24d884ff5
cory <cory@floop.org.uk>
2024-05-07 17:24:09 +0100

    Fix crashing on absence of api_key.txt

--- questionResponse.py
+++ questionResponse.py
@@ -7,8 +7,12 @@ class questionResponse:
 
     def __init__(self):
         # attempt to grab the api_key from api_key.txt
-        with open('api_key.txt', 'r') as file:
-            self.api_key = file.readline()
+        try:
+            with open('api_key.txt', 'r') as file:
+                self.api_key = file.readline()
+        except FileNotFoundError:
+            f = open('api_key.txt', 'x')
+            self.api_key = ""
 
     def get_response(self, questiontEXT):
         return 'bazinga'
